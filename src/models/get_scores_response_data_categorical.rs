c957bfdc266a365212824927313f6578f766227b
timvw <115225+timvw@users.noreply.github.com>
2025-11-13 03:19:39 +0000

    chore: update generated client from latest OpenAPI spec

--- src/models/get_scores_response_data_categorical.rs
+++ src/models/get_scores_response_data_categorical.rs
@@ -98,14 +98,9 @@ pub struct GetScoresResponseDataCategorical {
         skip_serializing_if = "Option::is_none"
     )]
     pub environment: Option<Option<String>>,
-    /// Only defined if a config is linked. Represents the numeric category mapping of the stringValue
-    #[serde(
-        rename = "value",
-        default,
-        with = "::serde_with::rust::double_option",
-        skip_serializing_if = "Option::is_none"
-    )]
-    pub value: Option<Option<f64>>,
+    /// Represents the numeric category mapping of the stringValue. If no config is linked, defaults to 0.
+    #[serde(rename = "value")]
+    pub value: f64,
     /// The string representation of the score value. If no config is linked, can be any string. Otherwise, must map to a config category
     #[serde(rename = "stringValue")]
     pub string_value: String,
@@ -121,6 +116,7 @@ impl GetScoresResponseDataCategorical {
         timestamp: String,
         created_at: String,
         updated_at: String,
+        value: f64,
         string_value: String,
     ) -> GetScoresResponseDataCategorical {
         GetScoresResponseDataCategorical {
@@ -140,7 +136,7 @@ impl GetScoresResponseDataCategorical {
             config_id: None,
             queue_id: None,
             environment: None,
-            value: None,
+            value,
             string_value,
             trace: None,
         }
