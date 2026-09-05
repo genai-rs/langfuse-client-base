d8c2df88975db1d00c089989875b718a6d443f62
timvw <115225+timvw@users.noreply.github.com>
2026-09-05 06:53:21 +0000

    chore: update generated client from latest OpenAPI spec

--- src/models/string_evaluation_rule_filter_1.rs
+++ src/models/string_evaluation_rule_filter_1.rs
@@ -1,7 +1,7 @@
 /*
  * server
  *
- * ## Authentication  Authenticate with the API using [Basic Auth](https://en.wikipedia.org/wiki/Basic_access_authentication), get API keys in the project settings:  - username: Langfuse Public Key - password: Langfuse Secret Key  ## Exports  - OpenAPI spec: https://cloud.langfuse.com/generated/api/openapi.yml
+ * ## Authentication  Authenticate with the API using [Basic Auth](https://en.wikipedia.org/wiki/Basic_access_authentication), get API keys in the project settings:  - username: Langfuse Public Key - password: Langfuse Secret Key  ## Exports  - OpenAPI spec: https://cloud.langfuse.com/generated/api/openapi.yml - Self-hosted deployments: interactive API reference at `/api/docs` and OpenAPI spec at `/api/openapi.yaml`
  *
  * The version of the OpenAPI document:
  *
